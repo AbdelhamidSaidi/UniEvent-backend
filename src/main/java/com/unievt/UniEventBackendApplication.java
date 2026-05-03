31d5aa268b6e42f9a440382546876e795f6f0fc9
Anas Rifak <anasrifakofficial@gmail.com>
2026-05-03 22:52:45 +0100

    initial commit

---
+++ src/main/java/com/unievt/UniEventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unievt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniEventBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unievt/UniEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unievt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
